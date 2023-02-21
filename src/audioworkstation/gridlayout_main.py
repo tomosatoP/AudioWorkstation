d1da2a68f447811ad493d1dc2e59e54b1b9442c3
tomosatoP <tomosato.g@gmail.com>
2023-02-21 16:23:07 +0900

    gridlayout

---
+++ src/audioworkstation/gridlayout_main.py
@@ -0,0 +1,21 @@
+import kivy  # noqa: F401
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+from . import gridlayouts  # noqa: F401
+
+
+class main_kv(GridLayout):
+    pass
+
+
+class Gridlayout_mainApp(App):
+    def build(self):
+        self.x = 800 / 4
+        self.y = 400 * 0.8
+        return main_kv()
+
+
+if __name__ == "__main__":
+    Gridlayout_mainApp().run()
